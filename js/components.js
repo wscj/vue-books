27c7864ea10370a34739eeefed9c1576922d910b
ChenJing <tadashi-chen@qq.com>
2017-10-11 16:29:49 +0800

    引入vue-resource并实现获取后台书籍列表

--- js/components.js
+++ js/components.js
@@ -1,69 +1,57 @@
 const bookList = {
   data () {
     return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }]
+      tableData: []
     }
   },
   methods: {
-    handleedit(index, row) {
+    handleEdit(index, row) {
       console.log(index, row);
     },
-    handledelete(index, row) {
+    handleDelete(index, row) {
+      console.log(index, row);
+    },
+    handleRead(index, row) {
       console.log(index, row);
     }
   },
-  template:
-  `<el-table
-    :data="tableData"
-    border
-    style="width: 100%">
-    <el-table-column
-      label="日期"
-      width="180">
-      <template scope="scope">
-        <el-icon name="time"></el-icon>
-        <span style="margin-left: 10px">{{ scope.row.date }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column
-      label="姓名"
-      width="180">
-      <template scope="scope">
-        <el-popover trigger="hover" placement="top">
-          <p>姓名: {{ scope.row.name }}</p>
-          <p>住址: {{ scope.row.address }}</p>
-          <div slot="reference" class="name-wrapper">
-            <el-tag>{{ scope.row.name }}</el-tag>
-          </div>
-        </el-popover>
-      </template>
-    </el-table-column>
-    <el-table-column label="操作">
-      <template scope="scope">
-        <el-button
-          size="small"
-          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-        <el-button
-          size="small"
-          type="danger"
-          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>`
+  template: `<el-table
+      :data="tableData"
+      border
+      style="width: 100%">
+      <el-table-column
+        prop="name"
+        label="书名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="category"
+        label="分类"
+        width="180">
+      </el-table-column>
+      <el-table-column label="操作">
+        <template scope="scope">
+          <el-button
+            size="small"
+            @click="handleRead(scope.$index, scope.row)">查看</el-button>
+          <el-button
+            size="small"
+            @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button
+            size="small"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>`,
+  created () {
+    this.$http.get('/api/books').then(
+      resp => {
+        this.tableData = resp.body.list
+      },
+      resp => {
+        console.error('fail')
+      }
+    )
+  }
 }
